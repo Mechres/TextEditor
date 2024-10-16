6493640d8f997b9187dc702c2218fa45e605e1fc
Yağız <164776284+Mechres@users.noreply.github.com>
2024-10-16 09:29:13 +0300

    Add file open
    Home, end keys and file open.

--- kilo.c
+++ kilo.c
@@ -1,10 +1,16 @@
 /*** includes ***/
+
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <termios.h>
 
@@ -18,18 +24,32 @@ enum editorkey {
 	ARROW_LEFT = 1000,
 	ARROW_RIGHT,
 	ARROW_UP,
-	ARROW_DOWN
+	ARROW_DOWN,
+	DEL_KEY,
+	HOME_KEY,
+	END_KEY,
+	PAGE_UP,
+	PAGE_DOWN
 };
 
 /*** data ***/
+typedef struct erow {
+	int size;
+	char *chars;
+} erow;
+
 struct editorConfig{
 	int cx, cy;
 	int screenrows;
 	int screencols;
+	int numrows;
+	erow row;
 	struct termios orig_termios;
 };
 
 struct editorConfig E;
+
+
 
 /*** terminal ***/
 
@@ -74,11 +94,34 @@ int editorReadKey(){
 	if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
 	if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 	if (seq[0] == '['){
+		if (seq[1] >= '0' && seq[1] <= '9'){
+			if(read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+			if(seq[2] == '~'){
+				switch(seq[1]) {
+					case '1': return HOME_KEY;
+					case '3': return DEL_KEY;
+					case '4': return END_KEY;
+					case '5': return PAGE_UP;
+					case '6': return PAGE_DOWN;
+					case '7': return HOME_KEY;
+					case '8': return END_KEY;
+				}
+			}
+		} else {
+		
 		switch (seq[1]){
 			case 'A': return ARROW_UP;
 			case 'B': return ARROW_DOWN;
 			case 'C': return ARROW_RIGHT;
 			case 'D': return ARROW_LEFT;
+			case 'H': return HOME_KEY;
+			case 'F': return END_KEY;
+		}
+	}
+	} else if(seq[0] == 'O'){
+		switch (seq[1]){
+			case 'H': return HOME_KEY;
+			case 'F': return END_KEY;
 		}
 	}
 	return '\x1b';	
@@ -119,6 +162,30 @@ int getWindowSize(int *rows, int *cols){
 		return 0;
 	}
 }
+
+/*** file i/o ***/
+void editorOpen(char *filename){
+	FILE *fp = fopen(filename, "r");
+	if (!fp) die("fopen");
+
+	char *line = NULL;
+	size_t linecap = 0;
+	ssize_t linelen;
+	linelen = getline(&line, &linecap, fp);
+	if (linelen != -1){
+		while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
+		linelen--;
+		
+	
+	E.row.size = linelen;
+	E.row.chars = malloc(linelen + 1);
+	memcpy(E.row.chars, line, linelen);
+	E.row.chars[linelen] = '\0';
+	E.numrows = 1;	
+	}
+	free(line);
+	fclose(fp);
+}
 /*** append buffer ***/
 
 struct abuf {
@@ -147,7 +214,8 @@ void abFree(struct abuf *ab){
 void editorDrawRows(struct abuf *ab){
 	int y;
 	for (y = 0; y < E.screenrows; y++){
-		if(y == E.screenrows / 3){
+		if(y >= E.numrows){
+		if(E.numrows == 0 && y == E.screenrows / 3){ // Welcome screen fix
 		char welcome[80];
 		int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
 		if (welcomelen > E.screencols) welcomelen = E.screencols;
@@ -161,7 +229,11 @@ void editorDrawRows(struct abuf *ab){
 		} else {
 		abAppend(ab, "~", 1);
 		}
-
+		} else {
+		int len = E.row.size;
+		if(len > E.screencols) len = E.screencols;
+		abAppend(ab, E.row.chars, len);
+		}
 		abAppend(ab, "\x1b[K", 3);
 		if(y < E.screenrows - 1){
 			abAppend(ab, "\r\n", 2);
@@ -192,16 +264,24 @@ void editorRefreshScreen(){
 void editorMoveCursor(int key){
 	switch (key) {
 		case ARROW_LEFT:
-			E.cx--;
+			if (E.cx != 0){
+				E.cx--;
+			}
 			break;
 		case ARROW_RIGHT:
-			E.cx++;
+			if (E.cx != E.screencols - 1){
+				E.cx++;
+			}
 			break;
 		case ARROW_UP:
-			E.cy--;
+			if (E.cy != 0){
+				E.cy--;
+			}
 			break;
 		case ARROW_DOWN:
+			if (E.cy != E.screenrows -1) {
 			E.cy++;
+			}
 			break;
 	}
 
@@ -214,6 +294,21 @@ void editorProcessKeypress(){
 			write(STDOUT_FILENO, "\x1b[2J", 4);
 			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
+			break;
+		case HOME_KEY:
+			E.cx = 0;
+			break;
+		case END_KEY:
+			E.cx = E.screencols -1;
+			break;
+		
+		case PAGE_UP:
+		case PAGE_DOWN:
+			{
+			int times = E.screenrows;
+			while(times--)
+				editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+			}
 			break;
 		case ARROW_UP:
 		case ARROW_DOWN:
@@ -231,12 +326,17 @@ void editorProcessKeypress(){
 void initEditor(){
 	E.cx = 0;
 	E.cy = 0;
+	E.numrows = 0;
+
 	if(getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
-int main(){
+int main(int argc, char *argv[]){
 	enableRawMode();
 	initEditor();
+	if(argc >= 2){
+	editorOpen(argv[1]);
+	}
 	while(1){
 		editorRefreshScreen();
 		editorProcessKeypress();
